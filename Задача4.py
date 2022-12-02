c23d405384c6496f5ce91bed5259f7a5bd47f74d
Alfir <alfir2710@rambler.ru>
2022-12-02 15:08:35 +0500

    ДЗ к уроку4

---
+++ Задача4.py
@@ -0,0 +1,19 @@
+# Задана натуральная степень k.
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100)
+# многочлена и записать в файл (или вывести в терминал) многочлен степени k.
+
+#Пример:
+
+#- k = 2  => 2*x² + 4*x + 5
+#- k = 3  => 41*x^3 + 6*x² + 2*x + 98
+
+from random import randint
+max_val=100
+k = int(input('Введите натуральную степень k:'))
+koeff=[randint(0,max_val) for i in range(k)]+[randint(1,max_val)]
+poly='+'.join([f'{(j,"")[j==1]}x^{i}' for i, j in enumerate(koeff) if j][::-1])
+poly=poly.replace('x^1+', 'x+')
+poly=poly.replace('x^0', '')
+poly+=('','1')[poly[-1]=='+']
+poly=(poly, poly[:-2])[poly[-2:]=='^1']
+print(poly)
